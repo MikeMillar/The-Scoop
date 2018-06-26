e1be2039bd282d9f1bb7b330f6f42f933a7b83dd
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2018-06-26 17:23:28 -0600

    working on POST comments

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 let database = {
   users: {},
   articles: {},
-  nextArticleId: 1
+  nextArticleId: 1,
+  comments: {},
+  nextCommentId: 1
 };
 
 const routes = {
@@ -26,6 +28,19 @@ const routes = {
   },
   '/articles/:id/downvote': {
     'PUT': downvoteArticle
+  },
+  '/comments': {
+    'POST': createComment
+  },
+  '/comments/:id': {
+    'PUT': updateComment,
+    'DELETE': deleteComment
+  },
+  '/comments/:id/upvote': {
+    'PUT': upvoteComment
+  },
+  '/comments/:id/downvote': {
+    'PUT': downvoteComment
   }
 };
 
@@ -218,6 +233,48 @@ function downvoteArticle(url, request) {
   }
 
   return response;
+}
+
+function createComment(url, request) {
+  const requestComment = request.body && request.body.comment;
+  const response = {};
+
+  if (requestComment && requestComment.body && requestComment.username && database.users[requestComment.username] && requestComment.articleId) {
+    const comment = {
+      id: database.nextCommentId++,
+      articleId: requestComment.articleId,
+      username: requestComment.username,
+      body: requestComment.body,
+      upvotedBy: [],
+      downvotedBy: []
+    };
+
+    database.comment[comment.id] = comment;
+    database.users[comment.username].commentIds.push(comment.id);
+
+    response.body = {comment: comment};
+    response.status = 201;
+  } else {
+    response.status = 400;
+  }
+  console.log(response);
+  return response;
+}
+
+function updateComment(url, request) {
+
+}
+
+function deleteComment(url, request) {
+
+}
+
+function upvoteComment(url, request) {
+
+}
+
+function downvoteComment(url, request) {
+
 }
 
 function upvote(item, username) {
